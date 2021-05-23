23812f6a8868650b537277ddab2a5866d85b7b6e
martin-tixcode <martin@tixcode.co>
2021-05-23 15:47:15 -0300

    endpoint

--- src/modules/db/index.ts
+++ src/modules/db/index.ts
@@ -1,40 +1,40 @@
-import fs from "fs";
-import path from "path";
-const Sequelize = require("sequelize");
-const Op = Sequelize.Op;
-const operatorsAliases = Object.entries(Op).reduce(
-  (acc: any, [name, value]) => {
-    acc[`$${name}`] = value;
+// import fs from "fs";
+// import path from "path";
+// const Sequelize = require("sequelize");
+// const Op = Sequelize.Op;
+// const operatorsAliases = Object.entries(Op).reduce(
+//   (acc: any, [name, value]) => {
+//     acc[`$${name}`] = value;
     
-    return acc;
-  },
-  {}
-);
+//     return acc;
+//   },
+//   {}
+// );
 
-const db: any = {};
-const {
-  DBURL
-} = require('../../../config/index');
+// const db: any = {};
+// const {
+//   DBURL
+// } = require('../../../config/index');
 
-const sequelize = new Sequelize(DBURL);
+// const sequelize = new Sequelize(DBURL);
 
-fs.readdirSync(`${__dirname}/models`).forEach(file => {
-  const model = sequelize.import(path.join(`${__dirname}/models/`, file));
-  db[model.name] = model;
-});
+// fs.readdirSync(`${__dirname}/models`).forEach(file => {
+//   const model = sequelize.import(path.join(`${__dirname}/models/`, file));
+//   db[model.name] = model;
+// });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+// Object.keys(db).forEach(modelName => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-db.Op = Op;
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+// db.Op = Op;
 
-sequelize.sync({
-  // force: true
-});
+// sequelize.sync({
+//   // force: true
+// });
 
-export default db;
+// export default db;
